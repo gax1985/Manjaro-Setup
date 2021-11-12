571474c0eadb27a69d947c696013d3af9743acee
Youssef <40011589+YoussefE95@users.noreply.github.com>
2021-11-12 10:51:16 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 sudo pacman -Syyu
 
 # install usual apps
-sudo pacman -S yay ranger neofetch cmatrix discord
-yay -S dropbox visual-studio-code-bin expressvpn spotify tutanota-desktop-bin todoist-appimage gotop-bin
+sudo pacman -S yay ranger neofetch cmatrix discord base-devel
+yay -S dropbox visual-studio-code-bin expressvpn spotify tutanota-desktop-bin todoist gotop-bin
 
 # install beautiful discord
 python3 -m pip install -U https://github.com/leovoel/BeautifulDiscord/archive/master.zip
